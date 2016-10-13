482e54bfe5ec1aca6863aa9e05d553b7222dcdb4
Ruy R. Garcia <rrgarciach@gmail.com>
2016-10-13 00:51:23 -0500

    Eliminated unused code from Menu controller.

--- src/menu/menu.controller.js
+++ src/menu/menu.controller.js
@@ -7,36 +7,4 @@ export default function MenuCtrl($scope, $ionicModal, $http) {
   //$scope.$on('$ionicView.enter', function(e) {
   //});
 
-  // Form data for the login modal
-  $scope.loginData = {};
-
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  })
-    .then(modal => {
-      $scope.modal = modal;
-    });
-
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function () {
-    $scope.modal.hide();
-  };
-
-  // Open the login modal
-  $scope.login = function () {
-    $scope.modal.show();
-  };
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function () {
-    console.log('Doing login', JSON.stringify($scope.loginData));
-
-    $http.post('https://gc-orders-development.herokuapp.com/api/v1/auth', $scope.loginData)
-      .then(function (response) {
-        console.log('token', JSON.stringify(response.data));
-        $scope.closeLogin();
-      });
-  };
-
 };
